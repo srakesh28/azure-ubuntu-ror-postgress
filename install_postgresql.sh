d9a398a5fb941ca00153a57bfce789b7574b4864
srakesh28 <srakesh28@yahoo.com>
2016-10-19 00:02:33 -0700

    uu

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -11,11 +11,11 @@ sudo echo mysql-server-5.5 mysql-server/root_password_again password $dbpass | d
 
 sudo apt-get -y install apache2
 
-wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/shared_scripts/ubuntu/vm-disk-utils-0.1.sh
-sudo chnmod a+x vm-disk-utils-0.1.sh
+#wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/shared_scripts/ubuntu/vm-disk-utils-0.1.sh
+#sudo chnmod a+x vm-disk-utils-0.1.sh
 
 #Format the data disk
-bash vm-disk-utils-0.1.sh -s
+#bash vm-disk-utils-0.1.sh -s
 
 # restart Apache
 sudo apachectl restart
