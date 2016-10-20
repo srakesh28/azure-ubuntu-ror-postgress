d6942f9e3656a0110d6c31f04f0ea66009bc3db8
srakesh28 <srakesh28@yahoo.com>
2016-10-19 23:25:14 -0700

    Update install_postgresql.sh

--- install_postgresql.sh
+++ install_postgresql.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2015 Microsoft Azure
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-#
-# Author: Full Scale 180 Inc.
-
-# You must be root to run this script
 if [ "${UID}" -ne 0 ];
 then
     log "Script executed without root permissions"
@@ -100,8 +74,8 @@ install_postgresql_service() {
 	# Install PostgreSQL if it is not yet installed
 	if [ $(dpkg-query -W -f='${Status}' postgresql 2>/dev/null | grep -c "ok installed") -eq 0 ];
 	then
-	  # apt-get -y install postgresql=9.3* postgresql-contrib=9.3* postgresql-client=9.3*
-	  apt-get -y install postgresql-9.3-postgis-2.1
+	  apt-get -y install postgresql=9.3* postgresql-contrib=9.3* postgresql-client=9.3*
+	  #apt-get -y install postgresql-9.3-postgis-2.1
 	fi
 	
 	logger "Done installing PostgreSQL..."
@@ -208,11 +182,11 @@ configure_streaming_replication() {
 }
 
 # MAIN ROUTINE
-#install_postgresql_service
+install_postgresql_service
 
 setup_datadisks
 
-#service postgresql start
+service postgresql start
 
 #configure_streaming_replication
 
