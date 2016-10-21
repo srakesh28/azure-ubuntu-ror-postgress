a6ee273913b6fcb6a1420f640a1c186d03271983
srakesh28 <srakesh28@yahoo.com>
2016-10-20 21:37:18 -0700

    new

---
+++ empty_script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# You must be root to run this script
+if [ "${UID}" -ne 0 ];
+then
+    log "Script executed without root permissions"
+    echo "You must be root to run this program." >&2
+    exit 3
+fi
+
+# uncomment this if you want to use data disk 
+# download file to format data disk
+
+# wget https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/shared_scripts/ubuntu/vm-disk-utils-0.1.sh
+# sudo chmod a+x vm-disk-utils-0.1.sh
+
+#Format the data disk
+#bash vm-disk-utils-0.1.sh -s
+# data disk setup ends here
+
+# TEMP FIX - Re-evaluate and remove when possible
+# This is an interim fix for hostname resolution in current VM (If it does not exist add it)
+grep -q "${HOSTNAME}" /etc/hosts
+if [ $? == 0 ];
+then
+  echo "${HOSTNAME}found in /etc/hosts"
+else
+  echo "${HOSTNAME} not found in /etc/hosts"
+  # Append it to the hsots file if not there
+  echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
+fi
+
+# Get today's date into YYYYMMDD format
+now=$(date +"%Y%m%d")
+
+install_pkg_service() {
+	logger "Start packages updates ..."
+	# Re-synchronize the package index files from their sources. An update should always be performed before an upgrade.
+	apt-get -y update
+
+	logger "Done packages updates ..."
+}
+
+# MAIN ROUTINE
+install_pkg_service
